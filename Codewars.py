e2970c40e51ebc88e3fa5ba89123cb045bab9b62
KleinerMarcel <Scott19Kirkiand@yandex.ru>
2022-09-22 14:48:33 +0300

    Работа с листами и заменой значений в них

--- Codewars.py
+++ Codewars.py
@@ -1,1 +1,9 @@
-vgvyucvu
+lst = [(3, 10), (5, 6), (45, 4)]
+lst = [i[0]+i[1] for i in lst]
+lst2 = []
+for i in lst:
+    if i >=16:
+        lst2.append('Senior')
+    else:
+        lst2.append('Open')
+print(lst2)
