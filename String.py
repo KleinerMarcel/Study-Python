073ba71a7a1039330550806fcb92edab9808fa70
KleinerMarcel <Scott19Kirkiand@yandex.ru>
2022-09-25 17:58:06 +0300

    Хороший поиск количества символов в строке

--- String.py
+++ String.py
@@ -1,4 +1,3 @@
 name = str(input())
-n = name.count('a') + name.count('u') + name.count('i') + name.count('o') + name.count('E') + name.count('U') + name.count('I') + name.count('O') + name.count('A')
+n = sum(1 for let in name if let in "aeiouAEIOU")
 print(n)
-
