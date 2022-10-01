28ddf065e3daaf1c2d57f5c2066af39d367d5fd6
KleinerMarcel <Scott19Kirkiand@yandex.ru>
2022-10-01 19:30:14 +0300

    Работа с квадратами

--- Math.py
+++ Math.py
@@ -1,17 +1,7 @@
-def series_sum(n):
-    return '{:.2f}'.format(sum(1.0/(3 * i + 1) for i in range(n)))
-print(series_sum (1) )
-
-n = 2
-s = 0.0
-n = range(0,n)
-for i in n:
-    s += 1/(1+3*float(i))
-print(s)
-
-def series_sum(n):
-    s = 0.0
-    n = range(0,n)
-    for i in n:
-        s += 1/(1+3*float(i))
-    return '{:.2f}'.format(s)
+n = 25
+h = int(n**0.5)
+if n/h == h :
+    n = n + 2*h+1
+    print(n)
+else:
+    print(-1)
