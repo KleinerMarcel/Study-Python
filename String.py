af6506382d231fecb0689b964f39f4cb51ae69ce
KleinerMarcel <Scott19Kirkiand@yandex.ru>
2022-10-22 19:17:27 +0300

    Подсчет элементов

--- String.py
+++ String.py
@@ -1,13 +1,5 @@
-"""
-('rock', 'scissors'), "Player 1 won!")
-'scissors', 'rock'), "Player 2 won!")
-"""
-
-
-def rps(p1, p2):
-    beats = {'rock': 'scissors', 'scissors': 'paper', 'paper': 'rock'}
-    if beats[p1] == p2:
-        return "Player 1 won!"
-    if beats[p2] == p1:
-        return "Player 2 won!"
-    return "Draw!"
+from collections import Counter
+word = "hello"
+word = word.lower()
+counter = Counter(word)
+print(counter, ''.join(('(' if counter[c] == 1 else ')') for c in word))
