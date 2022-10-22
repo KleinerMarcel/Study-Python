0df04877f7ad6edb63b7a203f1bbe7bb7909da3d
KleinerMarcel <Scott19Kirkiand@yandex.ru>
2022-10-22 15:58:01 +0300

    Черепашка!

--- Math.py
+++
@@ -1,8 +0,0 @@
-n = 11
-d = 15
-w = 30
-if n > 1:
-    l = (d*100*(n-1))+(w*(n-2))
-    print(l)
-else:
-    print(0)
---
+++ dictoinary.py
@@ -0,0 +1,3 @@
+m = 'green'
+d = {'red':'green','green':'yellow','yellow':'red' }
+print(d.get(m))
