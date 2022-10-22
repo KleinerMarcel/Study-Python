9ab74cead1a7d125da1de60705b4d89e9b6868ed
KleinerMarcel <Scott19Kirkiand@yandex.ru>
2022-10-22 16:15:34 +0300

    Черепашка!

---
+++ Class.py
@@ -0,0 +1,5 @@
+class Solution:
+    def __init__(self):
+        self.n = 10
+    def print_n(self):
+        print(self.n)
---
+++ Turtle.py
@@ -0,0 +1,9 @@
+import math
+def square(k):
+    import turtle
+    turtle.shape("turtle")
+    for i in range(5):
+        turtle.forward(k)
+        turtle.right(144)
+    turtle.exitonclick()
+print(square(200))
